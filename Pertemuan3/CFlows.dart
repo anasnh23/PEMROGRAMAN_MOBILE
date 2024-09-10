71a5dc362e2dc4f0f67f365ac9ef76d2c9574e27
anasnh12 <anasnurhidayat70@gmail.com>
2024-09-10 10:14:01 +0700

    Pratikum1-CFlows

---
+++ Pertemuan3/CFlows.dart
@@ -0,0 +1,20 @@
+void main() {
+  // String test = "test2";
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else if (test == "test2") {
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
+
+  // if (test == "test2") print("Test2 again");
+
+  bool test = true; // Menggunakan tipe data boolean
+
+  if (test) {
+    print("Kebenaran");
+  } else {
+    print("Bukan Kebenaran");
+  }
+}
