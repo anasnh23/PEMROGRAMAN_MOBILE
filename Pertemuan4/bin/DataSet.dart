7c6ae5269eb676ecaf0349a6656c3d381620e6d2
anasnh12 <anasnurhidayat70@gmail.com>
2024-09-17 11:39:51 +0700

    Pratikum2-Dataset

--- Pertemuan4/bin/DataSet.dart
+++ Pertemuan4/bin/DataSet.dart
@@ -1,6 +1,27 @@
 import 'package:dart_application_2/dart_application_2.dart' as dart_application_2;
 
-void main(List<String> arguments) {
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-print(halogens);
+// void main(List<String> arguments) {
+//   var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+// print(halogens);
+// }
+
+
+void main() {
+  var names1 = <String>{};
+  Set<String> names2 = {}; // This works, too.
+
+  // Menambahkan elemen menggunakan .add()
+  names1.add("Anas Nurhidayat");
+  names1.add("2241760069");
+
+  // Menambahkan elemen menggunakan .addAll()
+  names2.addAll(["Anas Nurhidayat", "2241760069"]);
+
+  print("names1:");
+  print(names1); 
+
+  print("names2:");
+  print(names2); 
 }
+
+
