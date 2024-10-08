2bf83a6d6cd1e66a97ca6f282c440176e9a832c3
anasnh12 <anasnurhidayat70@gmail.com>
2024-10-08 13:48:42 +0700

    update

--- flutter_latihan1/lib/main.dart
+++ flutter_latihan1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_latihan1/basic_widgets/image_widget.dart';
+import 'package:flutter_latihan1/basic_widgets/text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,9 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,  // Perbaikan di sini
